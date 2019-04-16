1e3319f0e0f08af4903af12e43bdb361aebd9429
Cory Gargas <cbgargas@uark.edu>
2019-04-16 15:32:24 -0500

    init commit

---
+++ LTPs132/csv-to-matrix.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+
+import os
+import argparse
+
+# create a column of taxa names in csv file by matching taxa names 
+#in renamed tree file to taxa names in csv file of characters
+
+
---
+++ LTPs132/tree-tip-renamer.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+import os
+import argparse
+
+#pull in list of names pulled from fasta file
+
+#replace '>' with '' in names
+
+#create a dict from names, with the accession #'s as keys for each name
+
+#If line contains [accession #], replace with value from key
+
+
