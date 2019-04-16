2d3e749d58e336c48b74694d1e3a206168ef85b0
Cory Gargas <cbgargas@uark.edu>
2019-04-16 14:37:25 -0500

    added temp file creation and deletion

--- LTPs132/tree-tip-cleaner.py
+++ LTPs132/tree-tip-cleaner.py
@@ -1,10 +1,22 @@
 #! /usr/bin/python
 
-with open('LTPs132_SSU_tree-to-mess-with.nexus', 'r') as tree:
-    with open('output.txt', 'w') as out:
-        for line in tree:
-            if line.find(' '):
-                out.write(line.replace(' ', '_'))
-            else:
-                continue 
+import os
+import argparse
 
+with open('LTPs132_SSU_tree-to-mess-with.nexus', 'r') as in_tree:
+    with open('temp1.txt', 'w') as temp1:
+        for line in in_tree:
+            if line.find(','):
+                temp1.write(line.replace("'", ''))
+            else:
+                continue
+        with open('temp1.txt', 'r') as temp2:
+            with open('temp2.txt', 'w') as temp3:
+                for line in temp2:
+                    if line.find(','):
+                        temp3.write(line.replace(' ', '_'))
+                    else:
+                        continue
+                
+os.remove('temp1.txt')
+os.remove('temp2.txt')
